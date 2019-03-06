99f02aeb76f4a253a8791430d4537e212e3ae1fb
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-06 16:37:19 +0100

    Create and position footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../assets/css/footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const Home = props => {
   return (
     <Fragment>
       <Header />
       This is the home page
+      <Footer />
     </Fragment>
   );
 };
