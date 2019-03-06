9d563de063b49fb016aabe60d95a3825e650b7de
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-06 16:08:09 +0100

    Tweak header to include navigation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import '../assets/css/header.css';
 
 const Header = props => {
   return (
     <header className="app-header">
-      <h1>BeMentor.</h1>
+      <div className="app-header__top">
+        <h1>BeMentor.</h1>
+      </div>
+      <nav className="app-header__navigation">
+        <ul>
+          <li><Link to="#">Connect</Link></li>
+          <li><Link to="#">Contact</Link></li>
+          <li><Link to="#">Inbox</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 };
