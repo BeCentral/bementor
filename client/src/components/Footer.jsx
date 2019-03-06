87183500ec184bffae298ac1a367c5b425f33ddb
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-06 16:58:35 +0100

    Inlude social icons in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 import '../assets/css/footer.css';
 
 const Footer = () => {
   return (
     <footer className="app-footer">
-
+      <div className="app-footer__socials">
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faLinkedin} />
+      </div>
     </footer>
   );
 };
