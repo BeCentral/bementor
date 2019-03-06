09f18cf596109eda582415c58e716ff30b20a21b
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-06 17:31:52 +0100

    Add links to footer logos

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import okbeLogo from '../assets/images/logo/open-knowledge-belgium.svg';
 import becentralLogo from '../assets/images/logo/becentral.png';
+import { ExternalLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
@@ -16,8 +17,8 @@ const Footer = () => {
         <i><FontAwesomeIcon icon={faLinkedin} /></i>
       </div>
       <div className="app-footer__logos">
-        <img src={okbeLogo} alt="Open Knowledge Belgium logo" />
-        <img src={becentralLogo} alt="BeCentral logo" />
+        <a href="https://openknowledge.be" target="_blank"><img src={okbeLogo} alt="Open Knowledge Belgium logo" /></a>
+        <a href="https://becentral.org" target="_blank"><img src={becentralLogo} alt="BeCentral logo" /></a>
       </div>
     </footer>
   );
