14c7ee77fd6651a7ccd8529c2209fef3414a162f
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-06 17:16:51 +0100

    Add logos

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import okbeLogo from '../assets/images/logo/open-knowledge-belgium.svg';
+import becentralLogo from '../assets/images/logo/becentral.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 import '../assets/css/footer.css';
 
@@ -12,6 +14,10 @@ const Footer = () => {
         <FontAwesomeIcon icon={faTwitter} />
         <FontAwesomeIcon icon={faGithub} />
         <FontAwesomeIcon icon={faLinkedin} />
+      </div>
+      <div className="app-footer__logos">
+        <img src={okbeLogo} alt="Open Knowledge Belgium logo" />
+        <img src={becentralLogo} alt="BeCentral logo" />
       </div>
     </footer>
   );
