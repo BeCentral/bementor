1e6ee2b77eb68dc971b93b2869f6bb2272292398
toinne <anthony.meirlaen@gmail.com>
2019-03-17 00:23:06 +0100

    add seed script

---
+++ server/scripts/import.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Mentor = require('../src/model/mentor.model');
+
+require('dotenv').config();
+
+const run = async () => {
+  mongoose.Promise = global.Promise;
+  await mongoose.connect(process.env.MONGODB_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  });
+
+  const names = require('./mentors.json');
+
+  const models = names.map((rawMentor) => {
+    return new Mentor({
+      firstName: rawMentor.firstName,
+    });
+  });
+
+  const savePromises = models.map((model) => {
+      return model.save();
+  });
+
+  await Promise.all(savePromises);
+  await mongoose.disconnect();
+};
+
+run();
+
