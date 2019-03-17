052a8e475632f3478ee1f29915e46d1cd8feb97b
toinne <anthony.meirlaen@gmail.com>
2019-03-17 09:08:44 +0100

    add mentors page to list them all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Mentors from './components/Mentors';
 
 class App extends Component {
 
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/:path(|index|home|start)" component={Home} />
+          <Route path="/mentors" component={Mentors} />
           <Route render={() => <p>Page not found</p>} />
         </Switch>
       </BrowserRouter>
---
+++ client/src/components/Mentors.jsx
@@ -0,0 +1,53 @@
+import React, {Component, Fragment} from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Container from './Container';
+import SearchBar from './SearchBar';
+import {findMentors} from "../api/mentors";
+import MiniMentor from "./MiniMentor";
+import Wrapper from "./Wrapper";
+
+class Mentors extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mentors: []
+    }
+  }
+
+  componentDidMount() {
+    this.search('neural')
+  }
+
+  search = async (query) => {
+    const mentors = await findMentors(query);
+    this.setState({
+      mentors: mentors
+    })
+  };
+
+  render() {
+
+    const mentors = this.state.mentors.map((mentor, index) => {
+      return <MiniMentor key={mentor._id} {...mentor} />
+    });
+
+    return (
+      <Fragment>
+        <Header/>
+        <Container>
+          <h2>Find your Guru</h2>
+
+          <SearchBar onSearch={this.search}/>
+          <Wrapper>
+            {mentors}
+          </Wrapper>
+        </Container>
+        <Footer/>
+      </Fragment>
+    )
+  }
+}
+
+export default Mentors;
