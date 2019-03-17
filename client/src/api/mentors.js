efb7ba66e8162dd2324878c4eb1ec7a437aa0748
toinne <anthony.meirlaen@gmail.com>
2019-03-17 09:08:15 +0100

    add backend search logic

--- client/src/api/mentors.js
+++ client/src/api/mentors.js
@@ -3,3 +3,9 @@ const API_URL = 'http://localhost:4000';
 export const getMentors = () => {
   return fetch(`${API_URL}/mentor`).then(response => response.json());
 };
+
+export const findMentors = (query) => {
+  const params = new URLSearchParams();
+  params.append('text', query);
+  return fetch(`${API_URL}/mentor/search?${params.toString()}`).then(response => response.json());
+};
--- server/src/route/mentor.route.js
+++ server/src/route/mentor.route.js
@@ -2,4 +2,5 @@ const mentors = require('../controller/mentor.controller');
 
 module.exports = (app) => {
   app.get('/mentor', mentors.findAll);
+  app.get('/mentor/search', mentors.search)
 };
