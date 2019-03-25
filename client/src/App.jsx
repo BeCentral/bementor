659f825017cb83ab21fabe5b757d1abf3c69c42b
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-25 16:20:46 +0100

    Create Profile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Users from './components/Users';
+import Profile from './components/Pages/Profile';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/:path(|index|home|start)" component={Home} />
       <Route path="/connect" component={Users} />
+      <Route path="/profile" component={Profile} />
       <Route render={() => <p>Page not found</p>} />
     </Switch>
   </BrowserRouter>
---
+++ client/src/components/Pages/Profile/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppContainer from '../../Containers/AppContainer';
+
+const Profile = () => (
+  <AppContainer>
+    This is the home page
+  </AppContainer>
+);
+
+export default Profile;
