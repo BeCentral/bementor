da6d2fd3426f06448d5650a315daa557e0be39ef
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-25 17:10:03 +0100

    Pass temporary user to Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,28 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Pages/Home';
 import Connect from './components/Pages/Connect';
 import Profile from './components/Pages/Profile';
+import User from './models/User';
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/:path(|index|home|start)" component={Home} />
-      <Route path="/connect" component={Connect} />
-      <Route path="/profile" component={Profile} />
-      <Route render={() => <p>Page not found</p>} />
-    </Switch>
-  </BrowserRouter>
-);
+const App = () => {
+  const user = new User({
+    firstName: 'Michiel',
+    lastName: 'Leyman',
+    picture: 'https://avatars3.githubusercontent.com/u/15386836?s=460&v=4',
+    interests: ['Developing', 'Eating', 'Sleeping', 'Repeating'],
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    tagline: 'I make useful things for other humans'
+  });
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:path(|index|home|start)" component={Home} />
+        <Route path="/connect" component={Connect} />
+        <Route path="/profile" render={() => <Profile user={user} />} />
+        <Route render={() => <p>Page not found</p>} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
