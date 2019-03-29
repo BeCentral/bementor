526c2b2df9ba573c8adff1470d0d29134a8cc7a8
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-29 15:55:54 +0100

    Transform users api file to api class

---
+++ client/src/api/User.js
@@ -0,0 +1,15 @@
+import API from './API';
+
+class UserAPI extends API {
+  ENDPOINT = `${this.BASE_URL}/user`
+
+  get = () => fetch(this.ENDPOINT, this.getOptions('get')).then(this.handleResponse);
+
+  find = (query) => {
+    const params = new URLSearchParams();
+    params.append('text', query);
+    return fetch(`${this.ENDPOINT}/search?${params.toString()}`).then(response => response.json());
+  }
+}
+
+export default UserAPI;
