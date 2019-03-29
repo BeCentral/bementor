082fd44106c4e4eb13c3366cf15725027dec2567
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-29 15:40:45 +0100

    Create base API class

---
+++ client/src/api/API.js
@@ -0,0 +1,33 @@
+const API_URL = 'http://localhost:4000';
+
+class API {
+  constructor() {
+    this.BASE_URL = API_URL;
+    this.headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    };
+  }
+
+  handleResponse = async (response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.indexOf('application/json') !== -1) return response.json();
+    return response;
+  }
+
+  getOptions(method, body = null) {
+    const options = {
+      method: method.toUpperCase(),
+      headers: this.headers
+    };
+
+    if (body) options.body = JSON.stringify(body);
+
+    return options;
+  }
+}
+
+export default API;
