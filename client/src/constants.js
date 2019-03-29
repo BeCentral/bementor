2766d9699e9b7f4967fc340839776edde78d8fb0
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-29 15:49:43 +0100

    Add constants file

---
+++ client/src/constants.js
@@ -0,0 +1,9 @@
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+
+const REQUEST_STATUSES = {
+  INACTIVE: 0,
+  HAS_ERRORED: 1,
+  IS_LOADING: 2
+};
+
+export const { IS_LOADING, INACTIVE, HAS_ERRORED } = REQUEST_STATUSES;
