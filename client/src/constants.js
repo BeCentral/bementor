6a310c26131692bfdd2d5beb6eac0754be4f5e99
Michiel Leyman <michiel.leyman@gmail.com>
2019-03-30 20:27:23 +0100

    Export request states as object

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,13 +2,13 @@ import UserAPI from './api/User';
 
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
 
-const REQUEST_STATUSES = {
+export const REQUEST_STATES = {
   INACTIVE: 0,
   HAS_ERRORED: 1,
   IS_LOADING: 2
 };
 
-export const { IS_LOADING, INACTIVE, HAS_ERRORED } = REQUEST_STATUSES;
+export const { IS_LOADING, INACTIVE, HAS_ERRORED } = REQUEST_STATES;
 
 export const API = {
   user: new UserAPI()
