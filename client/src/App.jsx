5d3da9d27a60e7e3a34a33f51832f707b5e105fd
Michiel Leyman <michiel.leyman@gmail.com>
2019-04-05 20:57:55 +0200

    Add auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AuthContext from './context/auth-context';
 import Home from './components/Pages/Home';
 import Connect from './components/Pages/Connect';
 import Profile from './components/Pages/Profile';
@@ -23,16 +24,25 @@ class App extends Component {
     }
   }
 
+  setAuthenticatedUser = user => this.setState({ user });
+
   render() {
+    const authContext = {
+      user: this.state.user,
+      setAuthenticatedUser: this.setAuthenticatedUser
+    };
+
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/:path(|index|home|start)" component={Home} />
-          <Route path="/connect" component={Connect} />
-          <Route path="/profile/:userId" component={Profile} />
-          <Route render={() => <p>Page not found</p>} />
-        </Switch>
-      </BrowserRouter>
+      <AuthContext.Provider value={authContext}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/:path(|index|home|start)" component={Home} />
+            <Route path="/connect" component={Connect} />
+            <Route path="/profile/:userId" component={Profile} />
+            <Route render={() => <p>Page not found</p>} />
+          </Switch>
+        </BrowserRouter>
+      </AuthContext.Provider>
     );
   }
 }
