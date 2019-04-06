68431989eb74dfdcf550e76e67378801961cd822
Michiel Leyman <michiel.leyman@gmail.com>
2019-04-06 13:07:11 +0200

    Remove request state constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,14 +2,6 @@ import UserAPI from './api/User';
 
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
 
-export const REQUEST_STATES = {
-  INACTIVE: 0,
-  HAS_ERRORED: 1,
-  IS_LOADING: 2
-};
-
-export const { IS_LOADING, INACTIVE, HAS_ERRORED } = REQUEST_STATES;
-
 export const API = {
   user: new UserAPI()
 };
