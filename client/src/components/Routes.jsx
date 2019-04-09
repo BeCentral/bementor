4d15ce9b0aba298ef6db53a37a704a9e092f3bcc
Michiel Leyman <michiel.leyman@gmail.com>
2019-04-09 11:23:40 +0200

    Allow modal-based routing
    If the page is linked to with { state: { modal: true } }, the Modal will render *on top* of the previous component. This allows setting the url to /login, which just shows a modal. Pretty nifty 🔥

--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Connect from './Pages/Connect';
 import Profile from './Pages/Profile';
+import Login from './Pages/Auth/LoginForm';
 
 class Routes extends Component {
-  state = {}
+  previousLocation = this.props.location;
+
+  componentWillUpdate(nextProps) {
+    const { location } = this.props;
+
+    // set previousLocation if props.location is not modal
+    if (nextProps.history.action !== 'POP' && (!location.state || !location.state.modal)) {
+      this.previousLocation = this.props.location;
+    }
+  }
 
   render() {
+    const { location } = this.props;
+    const isModal = !!(
+      location.state
+      && location.state.modal
+      && this.previousLocation !== location
+    );
+
     return (
-      <Switch>
-        <Route path="/:path(|index|home|start)" component={Home} />
-        <Route path="/connect" component={Connect} />
-        <Route path="/profile/:userId" component={Profile} />
-        <Route render={() => <p>Page not found</p>} />
-      </Switch>
+      <>
+        <Switch location={isModal ? this.previousLocation : location}>
+          <Route path="/:path(|index|home|start)" component={Home} />
+          <Route path="/connect" component={Connect} />
+          <Route path="/profile/:userId" component={Profile} />
+          <Route render={() => <p>Page not found</p>} />
+        </Switch>
+        {isModal ? <Route path="/login" component={Login} /> : null}
+      </>
     );
   }
 }
+
+Routes.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  location: PropTypes.object.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  history: PropTypes.object.isRequired
+};
 
 export default Routes;
