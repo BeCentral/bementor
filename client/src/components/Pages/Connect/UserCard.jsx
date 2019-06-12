e17895c3d0c179ffedfe372f483e81d1df9d7126
Michiel Leyman <michiel.leyman@gmail.com>
2019-06-12 17:27:50 +0200

    Restyle user card

--- client/src/components/Pages/Connect/UserCard.jsx
+++ client/src/components/Pages/Connect/UserCard.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Badge } from 'evergreen-ui';
 import { Link } from 'react-router-dom';
 import User from '../../../models/User';
 
 import '../../../assets/css/user.css';
 
-const UserCard = ({ user }) => (
-  <article className="app-user-mini">
-    <Link to={`/profile/${user._id}`}>
-      <img alt="avatar" src={user.picture || `https://api.adorable.io/avatars/${user._id}`} />
-    </Link>
-    <Link className="seamless" to={`/profile/${user._id}`}>
-      <p>
-        {user.firstName} {user.lastName}
-      </p>
-      <p>
-        {user.bio}
-      </p>
-    </Link>
-  </article>
-);
+const UserCard = ({ user }) => {
+  const renderInterests = () => {
+    if (!user.interests || user.interests.length === 0) return null;
+    const $interests = user.interests.map(i => <li key={i.name}><Badge>{i.name}</Badge></li>);
+    return <ul className="user-card__interests">{$interests}</ul>;
+  };
+
+  const $interests = renderInterests();
+  return (
+    <article className="user-card">
+      <Link to={`/profile/${user._id}`}>
+        <img alt="User vatar" src={user.picture || `https://api.adorable.io/avatars/${user._id}`} />
+      </Link>
+      <Link className="seamless" to={`/profile/${user._id}`}>
+        <p>
+          {user.firstName} {user.lastName}
+        </p>
+      </Link>
+      {$interests}
+    </article>
+  );
+};
 
 UserCard.propTypes = {
   user: PropTypes.instanceOf(User).isRequired
