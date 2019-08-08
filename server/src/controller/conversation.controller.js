72a08bbf757e9816e42da5c549364baaecaac7ca
Michiel Leyman <michiel.leyman@gmail.com>
2019-08-08 11:05:15 +0200

    Prettify conversation controller

--- server/src/controller/conversation.controller.js
+++ server/src/controller/conversation.controller.js
@@ -2,82 +2,69 @@ const Conversation = require('../model/conversation.model');
 const Message = require('../model/message.model');
 
 exports.findAll = (req, res) => {
-
   Conversation.find({
-    $or: [
-      {mentee: req.user._id},
-      {mentor: req.user._id}
-    ]
-  }).populate('mentor')
+    $or: [{ mentee: req.user._id }, { mentor: req.user._id }]
+  })
+    .populate('mentor')
     .populate('mentee')
-    .then((conversations) => {
+    .then(conversations => {
       res.send(conversations);
     })
-    .catch((err) => {
-      res.status(500).send({message: err.message});
+    .catch(err => {
+      res.status(500).send({ message: err.message });
     });
 };
 
 exports.findOne = (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   Conversation.findById(id)
     .populate('mentor')
     .populate('mentee')
     .populate('messages')
-    .then((conversation) => {
+    .then(conversation => {
       res.send(conversation);
     })
-    .catch((err) => {
-      res.status(500).send({message: err.message});
+    .catch(err => {
+      res.status(500).send({ message: err.message });
     });
 };
 
-exports.message = (req, res) => {
-  addMessage(req)
-    .then((conversation) => {
-      res.send(conversation);
-    })
-    .catch((err) => {
-      res.status(500).send({message: err.message});
-    });
-};
-
-const addMessage = async (req) => {
-  const {id} = req.params;
-  let conversation = await Conversation.findById(id);
+const addMessage = async req => {
+  const { id } = req.params;
+  const conversation = await Conversation.findById(id);
 
   if (
-    conversation.mentor._id.toString() !== req.user.id
-    && conversation.mentee._id.toString() !== req.user.id
+    conversation.mentor._id.toString() !== req.user.id &&
+    conversation.mentee._id.toString() !== req.user.id
   ) {
-    throw 'Not authorized';
+    throw new Error('Not authorized');
   }
 
   let message = new Message({
-    text: req.body["message.text"]
+    text: req.body['message.text']
   });
 
   message = await message.save();
 
   conversation.messages.push(message);
 
-  return await conversation.save();
+  return conversation.save();
 };
 
-exports.initiate = async (req, res) => {
-  initiateConversation(req)
-    .then((conversation) => {
+exports.message = (req, res) => {
+  addMessage(req)
+    .then(conversation => {
       res.send(conversation);
     })
-    .catch((err) => {
-      res.status(500).send({message: err.message});
+    .catch(err => {
+      res.status(500).send({ message: err.message });
     });
 };
 
-const initiateConversation = async (req) => {
+const initiateConversation = async req => {
   let conversation = await Conversation.findOne({
     mentor: req.body.mentor,
-    mentee: req.user.id,
+    mentee: req.user.id
   })
     .populate('mentor')
     .populate('mentee')
@@ -88,7 +75,7 @@ const initiateConversation = async (req) => {
   }
 
   let message = new Message({
-    text: "Hey do you want to be my mentor ?"
+    text: 'Hey do you want to be my mentor ?'
   });
 
   message = await message.save();
@@ -99,5 +86,15 @@ const initiateConversation = async (req) => {
     messages: [message]
   });
 
-  return await conversation.save();
+  return conversation.save();
+};
+
+exports.initiate = async (req, res) => {
+  initiateConversation(req)
+    .then(conversation => {
+      res.send(conversation);
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message });
+    });
 };
